59a2ac44ceff9fd80ae38090ed011840ef32d939
Isek7 <bengabrielisek@gmail.com>
2024-02-06 11:51:28 -0800

    First commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,59 +5,50 @@ class Square:
     A class representing a square shape.
     
     Attributes:
-    - side: The side length of the square.
+    - width: The width of the square.
+    - height: The height of the square.
     """
 
-    def __init__(self, side=0):
+    def __init__(self, width=0, height=0):
         """
-        Initializes a square with the given side length.
+        Initializes a square with the given width and height.
         
         Args:
-        - side: The side length of the square.
+        - width: The width of the square.
+        - height: The height of the square.
         """
-        self.side = side
+        self.width = width
+        self.height = height
 
-    @property
-    def side(self):
-        return self._side
-
-    @side.setter
-    def side(self, value):
-        if value < 0:
-            raise ValueError("Side length must be non-negative")
-        self._side = value
-
-    @property
-    def area(self):
+    def area_of_my_square(self):
         """
         Calculates the area of the square.
         
         Returns:
         The area of the square.
         """
-        return self.side ** 2
+        return self.width * self.height
 
-    @property
-    def perimeter(self):
+    def perimeter_of_my_square(self):
         """
         Calculates the perimeter of the square.
         
         Returns:
         The perimeter of the square.
         """
-        return self.side * 4
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """
         Returns a string representation of the square.
         
         Returns:
-        A string representation of the square in the format 'side_length'.
+        A string representation of the square in the format 'width/height'.
         """
-        return str(self.side)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = Square(side=12)
+    s = Square(width=12, height=9)
     print(s)
-    print("Area:", s.area)
-    print("Perimeter:", s.perimeter)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
